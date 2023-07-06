5f2502df5c774eb6496796c86a85dab1508aa087
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-07-06 12:12:32 +0530

    initiated server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express= require("express");
+const { connection } = require("./Configs/db");
+
+const app= express();
+
+const cors= require("cors");
+const { UserRouter } = require("./Routes/User.Route");
+
+app.use(cors({
+    origin: "*"
+}))
+
+
+app.use(express.json());
+require("dotenv").config();
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to homepage");
+})
+
+app.use("/user", UserRouter);
+
+app.use(validator);
+
+
+
+app.listen(8080, async()=>{
+    try {
+        await connection;
+        console.log("Connected to the Db");
+    } catch (error) {
+        console.log({"error" : error});
+    }
+    console.log(`Running at port 8080`);
+})
